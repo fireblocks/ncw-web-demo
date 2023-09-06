db995b86c9eeddf7908c5967118f383af26a48c8
Amit Perelstein <amper@fireblocks.com>
2023-09-06 12:40:08 +0300

    Tx details

--- src/components/TransactionRow.tsx
+++ src/components/TransactionRow.tsx
@@ -1,13 +1,53 @@
 import React from "react";
 import { useAppStore } from "../AppStore";
-import { ITransactionData } from "../services/ApiService";
+import { ITransactionData, ITransactionDetails, TTransactionStatus } from "../services/ApiService";
 
 interface IProps {
   tx: ITransactionData;
 }
 
+const formatter = new Intl.RelativeTimeFormat(undefined, {
+  numeric: "auto",
+})
+
+const DIVISIONS: { amount: number, name: Intl.RelativeTimeFormatUnit }[] = [
+  { amount: 60, name: "seconds" },
+  { amount: 60, name: "minutes" },
+  { amount: 24, name: "hours" },
+  { amount: 7, name: "days" },
+  { amount: 4.34524, name: "weeks" },
+  { amount: 12, name: "months" },
+  { amount: Number.POSITIVE_INFINITY, name: "years" },
+]
+
+function formatTimeAgo(date: Date) {
+  let duration = (date.valueOf() - new Date().valueOf()) / 1000
+
+  for (let i = 0; i < DIVISIONS.length; i++) {
+    const division = DIVISIONS[i]
+    if (Math.abs(duration) < division.amount) {
+      return formatter.format(Math.round(duration), division.name)
+    }
+    duration /= division.amount
+  }
+}
+
+function statusColor(status: TTransactionStatus) {
+  switch (status) {
+    case "PENDING_SIGNATURE":
+      return "text-warning";
+    case "COMPLETED":
+      return "text-success";
+    case "FAILED":
+    case "CANCELLED":
+      return "text-error";
+    default:
+      return "text-info";
+  }
+}
+
 export const TransactionRow: React.FC<IProps> = ({ tx }) => {
-  const { fireblocksNCW } = useAppStore();
+  const { fireblocksNCW, walletId } = useAppStore();
   const isMountedRef = React.useRef<boolean>(false);
   const [inProgress, setInProgress] = React.useState<boolean>(false);
   const [errorStr, setErrorStr] = React.useState<string | null>(null);
@@ -44,11 +84,24 @@ export const TransactionRow: React.FC<IProps> = ({ tx }) => {
   };
 
   const label = inProgress ? "Signing..." : "Sign";
+  const isOutgoing = (tx: ITransactionDetails) => 
+    tx.source.walletId === walletId;
 
   return (
     <tr key={tx.id}>
       <td>{tx.id}</td>
-      <td>{tx.status}</td>
+      <td>{formatTimeAgo(new Date(tx.createdAt!))}</td>
+      <td>{tx.details?.assetId}</td>
+      <td>{tx.details?.operation}</td>
+      <td><div className={statusColor(tx.status)}>{tx.status}</div></td>
+      <td>{tx.details && isOutgoing(tx.details) ? 
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+        </svg> :
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 4.5l-15 15m0 0h11.25m-11.25 0V8.25" />
+        </svg>
+      }</td>
       <td>
         {tx.status === "PENDING_SIGNATURE" ? (
           <button
--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,10 +1,67 @@
-export type TTransactionStatus = "PENDING_SIGNATURE" | "SUBMITTED";
+export type TTransactionStatus = "PENDING_SIGNATURE" | "SUBMITTED" | "FAILED" | "COMPLETED" | "CANCELLED";
+
+export interface ITransferPeer {
+  id: string;
+  type: string;
+  name?: string;
+  walletId?: string;
+}
+
+export interface IAmountInfo {
+  amount?: string;
+  requestedAmount?: string;
+  netAmount?: string;
+  amountUSD?: string;
+}
+
+export interface IFeeInfo {
+  networkFee?: string;
+  serviceFee?: string;
+  gasPrice?: string;
+}
+
+export interface ITransactionDetails {
+  id: string; //	ID of the transaction.
+  assetId: string; //	Transaction asset.
+  source: ITransferPeer; //	Source of the transaction.
+  destination: ITransferPeer; //	Fireblocks supports multiple destinations for UTXO-based blockchains. For other blockchains, this array will always be composed of one element.
+  requestedAmount: number; //	The amount requested by the user.
+  amountInfo: IAmountInfo; //	Details of the transaction's amount in string format.
+  feeInfo: IFeeInfo; //	Details of the transaction's fee in string format.
+  amount: number; //	If the transfer is a withdrawal from an exchange, the actual amount that was requested to be transferred. Otherwise, the requested amount.
+  netAmount: number; //	The net amount of the transaction, after fee deduction.
+  amountUSD: number; //	The USD value of the requested amount.
+  serviceFee: number; //	The total fee deducted by the exchange from the actual requested amount (serviceFee = amount - netAmount).
+  treatAsGrossAmount: boolean; //	For outgoing transactions, if true, the network fee is deducted from the requested amount.
+  networkFee: number; //	The fee paid to the network.
+  createdAt: number; //	Unix timestamp.
+  lastUpdated: number; //	Unix timestamp.
+  status: string; //		The current status of the transaction.
+  txHash: string; //	Blockchain hash of the transaction.
+  index: number; //[optional] For UTXO based assets this is the vOut, for Ethereum based, this is the index of the event of the contract call.
+  subStatus: string; //		More detailed status of the transaction.
+  sourceAddress: string; //For account based assets only, the source address of the transaction. (Note: This parameter will be empty for transactions that are not: CONFIRMING, COMPLETED, or REJECTED/FAILED after passing CONFIRMING status.)
+  destinationAddress: string; //Address where the asset were transferred.
+  destinationAddressDescription: string; //Description of the address.
+  destinationTag: string; //Destination tag for XRP, used as memo for EOS/XLM, or Bank Transfer Description for the fiat providers: Signet (by Signature), SEN (by Silvergate), or BLINC (by BCB Group).
+  signedBy: string[]; // Signers of the transaction.
+  createdBy: string; //Initiator of the transaction.
+  rejectedBy: string; //User ID of the user that rejected the transaction (in case it was rejected).
+  addressType: string; //[ ONE_TIME, WHITELISTED ].
+  note: string; //Custom note of the transaction.
+  exchangeTxId: string; //If the transaction originated from an exchange, this is the exchange tx ID.
+  feeCurrency: string; //The asset which was taken to pay the fee (ETH for ERC-20 tokens, BTC for Tether Omni).
+  operation: string; //	Default operation is "TRANSFER".
+  numOfConfirmations: number; //The number of confirmations of the transaction. The number will increase until the transaction will be considered completed according to the confirmation policy.
+  extraParameters: any; // JSON object	Protocol / operation specific parameters.
+}
 
 export interface ITransactionData {
   id: string;
   status: TTransactionStatus;
   createdAt?: number;
   lastUpdated?: number;
+  details?: ITransactionDetails;
 }
 
 export interface ICreateWeb3ConnectionResponse {
@@ -164,7 +221,7 @@ export class ApiService {
     this._pollingTxsActive.set(deviceId, true);
     let startDate = 0;
     while (!this._disposed) {
-      const response = await this._getCall(`api/devices/${deviceId}/transactions?poll=true&startDate=${startDate}`);
+      const response = await this._getCall(`api/devices/${deviceId}/transactions?poll=true&startDate=${startDate}&details=true`);
       const txes = await response.json();
       if (txes && Array.isArray(txes)) {
         for (const txData of txes) {
