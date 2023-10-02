bc1ff51cdb5b870cf9cdb0e3586099202365b88f
Gil Amran <gil@fireblocks.com>
2023-10-02 12:39:33 +0300

    Added an easy way to debug ncw-js-sdk

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ncw-web-demo/',
+  base: "/ncw-web-demo/",
   plugins: [react()],
-})
+  optimizeDeps: {
+    exclude: ["@fireblocks/ncw-js-sdk"],
+  },
+});
