c0b874e9537573850eaf159dded0548686e95695
Vitaliy Paykov <vitaliy@fireblocks.com>
2023-12-27 11:03:12 +0200

    better vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/ncw-web-demo/",
-  plugins: [react(), splitVendorChunkPlugin(),],
+  plugins: [react(), splitVendorChunkPlugin()],
   server: {
     open: true,
     host: 'localhost',
+    hmr: true,
   },
   optimizeDeps: {
     exclude: ["@fireblocks/ncw-js-sdk", "tsl-apple-cloudkit"],
